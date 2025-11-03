1dac8a1dc41d69e85e2767ea0e3f3f07167f36cf
vcnt <thiago.vicente@ua.pt>
2025-11-03 18:33:43 +0000

    Now no need to start app in background for cucumber

--- src/test/java/org/hw1/cucumber/CucumberSpringConfiguration.java
+++ src/test/java/org/hw1/cucumber/CucumberSpringConfiguration.java
@@ -1,9 +1,28 @@
 package org.hw1.cucumber;
 
 import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.test.context.ContextConfiguration;
+import org.hw1.HW1Application;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 
 @CucumberContextConfiguration
-@ContextConfiguration
+@SpringBootTest(
+    classes = HW1Application.class,
+    webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
+)
+@TestPropertySource(properties = {
+    "server.port=8080",
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.h2.console.enabled=false"
+})
 public class CucumberSpringConfiguration {
+
+    @LocalServerPort
+    private int port;
+
+    public int getPort() {
+        return port;
+    }
 }
